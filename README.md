ba52f1606195c78c6f36e7e183afc0d8ba1af00a
BrunitoNFT <brunomembrado10@gmail.com>
2023-06-28 20:24:41 -0300

    No favorite card

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # finalfrontend
 
+Project link: https://finalfrontend-mku8.vercel.app/
+
 It's important to create a .env file with the following variables:
 
 VITE_API_KEY = https://jsonplaceholder.typicode.com/users
--- src/routes/Favorites.tsx
+++ src/routes/Favorites.tsx
@@ -8,9 +8,13 @@ import { dentists } from "../utils/types";
 import DentistsSkeleton from "../components/DentistsSkeleton";
 import DentistsCard from "../components/DentistsCard";
 
+import { useNavigate } from "react-router-dom";
+
 const fakeArr: string[] = new Array(30).fill("");
 
 const Favorites: React.FC = () => {
+  const navigate = useNavigate();
+
   const { dentists, loading } = useFetch();
 
   const { state, dispatch } = useContext(favoriteContext);
@@ -19,9 +23,8 @@ const Favorites: React.FC = () => {
   const handleButtonClick = (id: string) => {
     dispatch({ type: "toggleFav", payload: id });
   };
-
   return (
-    <main className="text-black w-full   flex flex-wrap gap-4 p-2  transition-all duration-500 justify-center   ">
+    <main className="text-black w-full h-full flex flex-wrap gap-4 p-2  transition-all duration-500 justify-center items-center ">
       {loading && fakeArr.map((_, index) => <DentistsSkeleton id={index} />)}
       {!loading &&
         dentists &&
@@ -38,6 +41,19 @@ const Favorites: React.FC = () => {
               favorites={favorites}
             />
           ))}
+      {!loading && Array.isArray(dentists) && favorites.length === 0 && (
+        <div className="flex flex-col items-center justify-center bg-white p-4 rounded-md shadow-xl dark:text-white">
+          <h1 className="text-2xl font-bold text-gray-500 dark:text-black ">
+            No favorite dentist :(
+          </h1>
+          <button
+            className="bg-blue-300 text-white p-2 rounded-md mt-4 font-thin dark:bg-white dark:text-black hover:scale-95 shadow-inner border"
+            onClick={() => navigate("/dentists")}
+          >
+            Add some dentists
+          </button>
+        </div>
+      )}
     </main>
   );
 };
